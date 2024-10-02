06b85a81fc81c9d538c28d94e4dbdd70a0412a1c
seenpie <bananananasez@gmail.com>
2024-10-02 20:09:04 +0300

    feat: implement streams section

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+import path from "path";
+import { EOL } from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await pipeline(createReadStream(filePath), process.stdout, { end: false });
+    process.stdout.write(EOL);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import {pipeline} from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files/fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const fileWriteStream = createWriteStream(filePath, { flags: "a" });
+
+  try {
+    await pipeline(process.stdin, fileWriteStream);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
