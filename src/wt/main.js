060559219265812b7c02138f7607f2e4fe81aa9c
seenpie <bananananasez@gmail.com>
2024-10-03 00:41:22 +0300

    feat: implement wt section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let startNum = 10;
+const numCPUs = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPromises = Array.from({ length: numCPUs }, () => {
+    return new Promise((resolve) => {
+      const worker = new Worker(path.join(__dirname, "worker.js"));
+
+      worker.postMessage(startNum++);
+
+      worker.on("message", (data) => {
+        resolve({ status: "resolved", data });
+        worker.terminate();
+      });
+
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+      })
+    })
+  });
+
+  const results = await Promise.all(workerPromises);
+  console.log(results);
 };
 
 await performCalculations();
