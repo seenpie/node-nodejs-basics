9c3a7b3f2952a1b1346014f8680323668140adbc
seenpie <bananananasez@gmail.com>
2024-10-02 20:50:52 +0300

    feat: implement zip section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream, promises as fs } from "fs";
+import { createGzip } from "zlib";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files/fileToCompress.txt");
+  const outputFilePath = path.join(__dirname, "files/archive.gz");
+
+  try {
+    await fs.access(filePath);
+
+    await pipeline(
+      createReadStream(filePath),
+      createGzip(),
+      createWriteStream(outputFilePath)
+    );
+
+    await fs.rm(filePath, { recursive: true });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream, promises as fs } from "fs";
+import { createUnzip } from "zlib";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const compressedFilePath = path.join(__dirname, "files/archive.gz");
+  const outputFilePath = path.join(__dirname, "files/fileToCompress.txt");
+
+  try {
+    await fs.access(compressedFilePath);
+
+    await pipeline(
+      createReadStream(compressedFilePath),
+      createUnzip(),
+      createWriteStream(outputFilePath)
+    );
+
+    await fs.rm(compressedFilePath, { recursive: true });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
